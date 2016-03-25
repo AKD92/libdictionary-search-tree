f9b1f3e1794efe4904ad8ee041916ce91fad460c
Ashis Kumar Das <akd.bracu@gmail.com>
2016-03-25 19:41:29 +0600

    Update bst_key_value_list.c

--- src/bst_key_value_list.c
+++ src/bst_key_value_list.c
@@ -32,14 +32,13 @@ int bst_keys(const BST *tree, List *keyList) {
 	n = list_head(keyList);
 	
 	/* Use Inorder algorithm to get a Sorted Key sequence (might be convenient) */
-	bst_inorder(tree, bst_root(tree), &nodeList);
+	bst_inOrder(tree, bst_root(tree), &nodeList);
 	
 	while (queue_size(&nodeList) > 0) {
 		
 		queue_dequeue(&nodeList, (void **) &node);
 		
 		if (bst_isInternal((const BNode *) node)) {
-			
 			list_ins_next(keyList, n, (const void *) node->key);
 			n = list_tail(keyList);
 		}
@@ -63,14 +62,13 @@ int bst_elements(const BST *tree, List *elementList) {
 	n = list_tail(elementList);
 	
 	/* Use Inorder algorithm to get a Sorted Element sequence (According to Keys) */
-	bst_inorder(tree, bst_root(tree), &nodeList);
+	bst_inOrder(tree, bst_root(tree), &nodeList);
 	
 	while (queue_size(&nodeList) > 0) {
 		
 		queue_dequeue(&nodeList, (void **) &node);
 		
 		if (bst_isInternal((const BNode *) node)) {
-			
 			list_ins_next(elementList, n, (const void *) node->element);
 			n = list_tail(elementList);
 		}
