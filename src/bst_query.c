aa6c2b1386bee1f4dc8c99ffeb7c847e12a53de0
Ashis Kumar Das <akd.bracu@gmail.com>
2016-03-25 19:42:33 +0600

    Update bst_query.c

--- src/bst_query.c
+++ src/bst_query.c
@@ -12,6 +12,14 @@
 
 
 #include "bst.h"
+#include <stack.h>
+#include <queue.h>
+
+
+
+
+#define maxu(a, b) ((a) > (b) ? (a) : (b))
+
 
 
 
@@ -28,28 +36,28 @@ unsigned int bst_size(const BST *tree) {
 
 BNode * bst_root(const BST *tree) {
 	
-	return tree == 0 ? 0 : tree->root;
+	return (tree == 0 ? 0 : tree->root);
 }
 
 
 
 BNode * bst_parent(const BNode *v) {
 	
-	return v == 0 ? 0 : v->parent;
+	return (v == 0 ? 0 : v->parent);
 }
 
 
 
 BNode * bst_leftChild(const BNode *v) {
 	
-	return v == 0 ? 0 : v->leftChild;
+	return (v == 0 ? 0 : v->leftChild);
 }
 
 
 
 BNode * bst_rightChild(const BNode *v) {
 	
-	return v == 0 ? 0 : v->rightChild;
+	return (v == 0 ? 0 : v->rightChild);
 }
 
 
@@ -60,10 +68,10 @@ BNode * bst_sibling(const BNode *v) {
 	BNode *parent;
 	
 	parent = bst_parent(v);
-	if (parent == 0) sibling = 0;
-	
+	if (parent == 0) {
+		sibling = 0;
+	}
 	else {
-		
 		if (v == bst_leftChild(parent)) {
 			sibling = bst_rightChild(parent);
 		}
@@ -79,12 +87,13 @@ BNode * bst_sibling(const BNode *v) {
 
 int bst_isExternal(const BNode *v) {
 	
-	BNode *left, *right;
+	unsigned int left, right;
 	
-	left = bst_leftChild(v);
-	right = bst_rightChild(v);
+	left = (unsigned int) bst_leftChild(v);
+	right = (unsigned int) bst_rightChild(v);
 	
-	if (((int)left | (int)right) == 0) {
+	if ((left | right) == 0) {
+	// if ((left == 0) & (right == 0)) {
 		return 1;
 	}
 	else {
@@ -96,12 +105,13 @@ int bst_isExternal(const BNode *v) {
 
 int bst_isInternal(const BNode *v) {
 	
-	BNode *left, *right;
+	unsigned int left, right;
 	
-	left = bst_leftChild(v);
-	right = bst_rightChild(v);
+	left = (unsigned int) bst_leftChild(v);
+	right = (unsigned int) bst_rightChild(v);
 	
-	if (((int)left | (int)right) != 0) {
+	if ((left | right) != 0) {
+	// if ((left != 0) | (right != 0)) {
 		return 1;
 	}
 	else {
@@ -120,3 +130,71 @@ int bst_isRoot(const BST *tree, const BNode *v) {
 		return 0;
 	}
 }
+
+
+
+unsigned int bst_depth(const BST *tree, const BNode *v) {
+	
+	register unsigned int iDepth;
+	register const BNode *nodeCurrent;
+	
+	iDepth = 0;
+	nodeCurrent = v;
+	
+	while (nodeCurrent != bst_root(tree)) {
+		nodeCurrent = bst_parent(nodeCurrent);
+		iDepth = iDepth + 1;
+	}
+	
+	return iDepth;
+}
+
+
+
+unsigned int bst_height(const BST *tree, const BNode *v) {
+	
+	unsigned int nodeHeight;
+	int isExternal;
+	Queue postOrder;
+	Stack stHeight;
+	unsigned int *heightN;
+	const unsigned int *heightR, *heightL;
+	const BNode *currentNode;
+	
+	queue_init(&postOrder, 0);
+	stack_init(&stHeight, free);
+	bst_postOrder(tree, (BNode *) v, &postOrder);
+	
+	while (queue_size(&postOrder) > 0) {
+		
+		queue_dequeue(&postOrder, (void **) &currentNode);
+		isExternal = bst_isExternal(currentNode);
+		
+		if (isExternal == 1) {
+			heightN = (unsigned int *) malloc(sizeof(unsigned int));
+			*heightN = 0;
+			stack_push(&stHeight, (const void *) heightN);
+		}
+		else {
+			stack_pop(&stHeight, (void **) &heightR);
+			stack_pop(&stHeight, (void **) &heightL);
+			
+			heightN = (unsigned int *) malloc(sizeof(unsigned int));
+			*heightN = 1 + maxu(*heightL, *heightR);
+			stack_push(&stHeight, (const void *) heightN);
+			
+			free((void *) heightR);
+			free((void *) heightL);
+		}
+	}
+	
+	stack_pop(&stHeight, (void **) &heightN);
+	nodeHeight = *heightN;
+	
+	stack_destroy(&stHeight);
+	queue_destroy(&postOrder);
+	free((void *) heightN);
+	
+	return nodeHeight;
+}
+
